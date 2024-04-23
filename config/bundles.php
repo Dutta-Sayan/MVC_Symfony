30600ef12b011373a171dd3d3ae3387eba58c66e
Dutta-Sayan <sayan.dutta@innoraft.com>
2024-04-23 14:33:54 +0530

    FT2-113: Capitalised the boolean values.

--- config/bundles.php
+++ config/bundles.php
@@ -1,16 +1,16 @@
 <?php
 
 return [
-    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
-    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
-    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true],
-    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
-    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
-    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
-    Symfony\UX\Turbo\TurboBundle::class => ['all' => true],
-    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
-    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => TRUE],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => TRUE],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => TRUE],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => TRUE],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => TRUE],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => TRUE, 'test' => TRUE],
+    Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => TRUE],
+    Symfony\UX\Turbo\TurboBundle::class => ['all' => TRUE],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => TRUE],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => TRUE],
+    Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => TRUE],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => TRUE],
 ];
