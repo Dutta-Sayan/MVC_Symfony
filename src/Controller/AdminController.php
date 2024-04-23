d59f4398c09c844ed3a5fc05bbb7cae53e89a9b1
Dutta-Sayan <sayan.dutta@innoraft.com>
2024-04-23 14:31:55 +0530

    FT2-113: Made the feature to display all students to Admin using Ajax.

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,13 +2,17 @@
 
 namespace App\Controller;
 
-use Services\Admin;
-use Services\Admin\NewExam;
-use Services\Admin\CalculateQues;
+use App\Services\Admin;
+use App\Services\Admin\NewExam;
+use App\Services\Admin\Students;
+use App\Services\Admin\CalculateQues;
 use App\Entity\Exam;
+use App\Entity\StudentProfile;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 /**
@@ -26,9 +30,16 @@ class AdminController extends AbstractController
   #[Route('/admin', name: 'admin')]
   public function index(): Response
   {
+    // Shows the admin dashboard only if the user is a admin.
+    if ($this->getUser()->getRole() == 'Admin') {
       return $this->render('admin/admin.html.twig', [
           'controller_name' => 'AdminController',
       ]);
+    }
+
+    else {
+      throw $this->createNotFoundException('Page does not exist');
+    }
   }
 
   /**
@@ -46,6 +57,7 @@ class AdminController extends AbstractController
   {
     if (isset($_POST['submit'])) {
       $newExam = new NewExam($_POST);
+      // Processes the new exam form.
       $newExam -> processForm($entityManager);
       return $this->redirectToRoute('existingExam');
     }
@@ -67,6 +79,7 @@ class AdminController extends AbstractController
   {
     if (isset($_POST['submit'])) {
       $adminService = new Admin\AdminServices($_POST);
+      // Processes the question form.
       $adminService->processForm($entityManager);
       $this->addFlash('success', 'Questions Set');
       return $this->redirectToRoute('admin');
@@ -90,12 +103,38 @@ class AdminController extends AbstractController
   public function existingExam(EntityManagerInterface $em): Response
   {
     $var = $this->getUser()->getEmail();
+    // Stores all the exams created by the user.
     $exam = $em->getRepository(Exam::class)->findBy(['CreatorEmail'=>$var]);
     $noOfQuestions = new CalculateQues();
+    // Stores the number of questions for each exam.
     $noOfQues = $noOfQuestions->calNoOfQues($exam, $em);
     return $this->render('admin/ExistingExams.html.twig', [
       'exam'=> $exam,
       'noOfQues'=>$noOfQues,
     ]);
+  }
+
+  /**
+   * Route for students list page.
+   *
+   * @param EntityManagerInterface $em
+   *  Object of EntityManagerInterface.
+   *
+   * @param Request $request
+   *  HTTP request parameter.
+   *
+   * @return Response
+   *  Returns the students list page.
+   */
+  #[Route('/admin/allStudents', name: 'showStudents')]
+  public function showStudents(Request $request, EntityManagerInterface $em): Response
+  {
+    if ($request->isXmlHttpRequest() || $request->query->get('showJson') == 1) {
+      $showStudents = new Students();
+      // Stores all the student users.
+      $jsonData = $showStudents->showStudents($em, $request);
+      return new JsonResponse($jsonData);
+    }
+    return $this->render('admin/showStudents.html.twig');
   }
 }
--- src/Services/Admin/Students.php
+++ src/Services/Admin/Students.php
@@ -3,6 +3,9 @@
 namespace App\Services\Admin;
 
 use App\Entity\StudentProfile;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Doctrine\ORM\EntityManagerInterface;
 
 /**
@@ -19,9 +22,26 @@ class Students
    * @return array
    *  Returns the recors of Students entity.
    */
-  public function showStudents(EntityManagerInterface $em) : array
+  public function showStudents(EntityManagerInterface $em, Request $request) : array
   {
     $students = $em->getRepository(StudentProfile::class)->findAll();
-    return $students;
+    $jsonData = array();
+    $idx = 0;
+
+    // Loops through each student record.
+    foreach ($students as $student) {
+      $temp = array(
+        'name'=>$student->getName(),
+        'email'=>$student->getStudentEmail()->getEmail(),
+        'age'=>$student->getAge(),
+        'roll'=>$student->getRollNo(),
+        'gradYear'=>$student->getGraduationYear(),
+        'marks'=>$student->getAcademicMarks(),
+        'link'=>$student->getLinkToResume(),
+      );
+      $jsonData[$idx++] = $temp;
+    }
+    
+    return $jsonData;
   }
 }
